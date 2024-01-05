4254e6562aa1c7a122a4305ce500d2e3b8eac6cb
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-01-05 11:06:39 +0530

    Old Text Analyzer

--- src/App.js
+++ src/App.js
@@ -3,24 +3,13 @@ import React from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutTxt="Home" homeTxt="About"/>
-      <div className="container my-10">
-        <router>
-          <Switch>
-            <Route exact path="/" TextForm component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </router>
+
         <TextForm
           heading="Enter The Text :"
           TextForm
@@ -29,8 +18,8 @@ function App() {
           btn3="Clear Text"
           btn4="Click to Listen Text"
         />
-        {/* <About/> */}
-      </div>
+        <About/>
+    
     </>
   );
 }
